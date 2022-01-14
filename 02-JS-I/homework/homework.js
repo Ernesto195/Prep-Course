e7a34de5b1ddf3987d25fb722a6201670dd2c929
ernesto <ernestofernado_@hotmail.com>
2022-01-14 18:20:06 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,7 +27,7 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  return str;
+  return 'ernesto';
 }
 
 function suma(x, y) {
