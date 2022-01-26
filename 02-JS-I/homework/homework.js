f31244959951e157fdb8b3786afd44eec3c8b7a2
ernesto <ernestofernado_@hotmail.com>
2022-01-26 03:01:22 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -106,14 +106,14 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  return num*num 
+  
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return num*num
 }
 
 function elevarAlCubo(num) {
